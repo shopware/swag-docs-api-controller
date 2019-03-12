bf8930e15f4e8d3905f70c870c18ba8206801eca
Patrick Stahl <p.stahl@shopware.com>
2019-03-12 15:41:27 +0100

    Initial Commit

---
+++ Controller/MyController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace ApiController\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class MyController extends AbstractController
+{
+    /**
+     * @Route("/api/v{version}/swag/my-api-action", name="api.action.swag.my-api-action", methods={"GET"})
+     */
+    public function myFirstApi(): JsonResponse
+    {
+        return new JsonResponse(['You successfully created your first controller route']);
+    }
+}
